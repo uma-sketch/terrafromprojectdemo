70b110f15b975cbc0bfa914120a2bb7db0f7f581
uma-sketch <main.vertex5@gmail.com>
2026-01-26 07:36:02 +0530

    Use Standard_D2s_v3 with single VM to fit Central India quota

--- project2/modules/windows-vm/variables.tf
+++ project2/modules/windows-vm/variables.tf
@@ -4,7 +4,8 @@ variable "location" {}
 variable "resource_group_name" {}
 variable "subnet_id" {}
 variable "lb_backend_pool_id" {}
-variable "vm_size" { default = "Standard_B1s" }
+variable "vm_size" { default = "Standard_D2s_v3" }
 variable "admin_username" {}
 variable "admin_password" { sensitive = true }
+
 
